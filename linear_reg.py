2c353c2045126e0deb5204206cf478942a566202
shovan777 <shovan@fusemachines.com>
2017-11-17 15:20:08 +0545

    Use optimizer module

--- linear_reg.py
+++ linear_reg.py
@@ -2,43 +2,8 @@
 
 import numpy as np
 import pandas as pd
-import compute_cost
 import matplotlib.pyplot as plt
-
-
-def gradient_descent(X, Y, theta, alpha, iterations, normal_factor):
-    """Apply batch gradient_descent.
-
-    Parameters
-    ----------
-    X : np.matrix
-        Value of features.
-    Y : np.matrix
-        Target Value.
-    theta : np.matrix
-        Feature matrix.
-    alpha : float
-        Learning rate.
-    iterations : int
-        Number of epochs.
-
-    Returns
-    -------
-    np.matrix
-        Optimized weights.
-
-    """
-    m = Y.shape[0]
-    print(m)
-    J_accumulate = np.zeros((iterations, 1), dtype=np.float64)
-    for i in range(iterations):
-        # print(theta)
-        theta = theta - alpha * (1. / m) * np.dot(X.T, (np.dot(X, theta) - Y))
-        J_accumulate[i] = compute_cost.compute_cost(X, Y, theta)
-#     return theta, J_accumulate
-    theta[0] = theta[0] * normal_factor
-    return theta, J_accumulate
-
+import optimizer
 
 iterations = 10000
 alpha = 0.5
@@ -64,7 +29,7 @@ Py = Y
 Y = Y / normal_factor
 Y = np.asmatrix(Y)
 Y = Y.T
-theta, J_history = gradient_descent(
+theta, J_history = optimizer.gradient_descent(
     X, Y, theta, alpha, iterations, normal_factor)
 print("optimized theta [A B]:\n>>", theta)
 print("\nfinal_cost:J_history[-1]:\n>>", J_history[-1])
